3bd16bed6311aa6fd118891b55a02b9171eca832
subarssini <subarssini@gmail.com>
2026-06-22 00:24:30 +0530

    LeetCode #2529

---
+++ maximum_count_of_positive_and_negative_integer_2529.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        pos=0
+        neg=0
+        for i in nums:
+            if i>0:
+                pos+=1
+            if i<0:
+                neg+=1
+        if pos>neg:
+            return pos
+        else:
+            return neg
+                
+        
